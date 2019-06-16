fe35fc9c41fb160161816fa767935afa96431ab1
ankitmarothi1610 <ankitmarothi1610@gmail.com>
2019-06-17 00:21:33 +0530

    Code changes to handle job scheduling
    Job scheduling completed at first level, unit testing done on cycle
    detection. Edge cases yet to be covered.

--- src/Resolver/JobDependencyResolver.java
+++ src/Resolver/JobDependencyResolver.java
@@ -5,18 +5,56 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jgrapht.alg.cycle.CycleDetector;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.traverse.DepthFirstIterator;
+
 public class JobDependencyResolver {
-	List<String> inputList;
-	List<String> resultList;
-
+	List<String[]> inputList;
+	
+	
 	public JobDependencyResolver() {
-		inputList = new ArrayList<String>(); 
+		inputList = new ArrayList<String[]>();
 	}
 
-	public void printResult() {
-
+	public void printResult(List<String> resultList) {
+		if (resultList == null || resultList.isEmpty()) {
+			System.out.println("Empty Sequence");
+		} else {
+			System.out.print("Job Sequence: " );
+			for (String result: resultList) {
+				System.out.print(" " + result);
+			}
+		}
 	}
 
+	public boolean detectCycle(DefaultDirectedGraph<String, DefaultEdge> directedGraph) {
+		CycleDetector<String, DefaultEdge> cycleDetector 
+	      = new CycleDetector<String, DefaultEdge>(directedGraph);
+	    return cycleDetector.detectCycles();
+	}
+	
+	public List<String> scheduleJobs(List<String[]> inputList) {
+		List<String> resultList = new ArrayList<String>(inputList.size());
+		DefaultDirectedGraph<String, DefaultEdge> directedGraph = new DefaultDirectedGraph<>(DefaultEdge.class);
+		DepthFirstIterator depthFirstIterator = null;
+		for (String[] vertices: inputList) {
+			directedGraph.addVertex(vertices[0]);
+			directedGraph.addVertex(vertices[1]);
+			directedGraph.addEdge(vertices[1], vertices[0]);
+		}
+		if (detectCycle(directedGraph)) {
+			System.out.println("ERROR: The jobs cannot depend on themselves");
+			return null;
+		}
+		depthFirstIterator = new DepthFirstIterator<>(directedGraph);
+		while(depthFirstIterator.hasNext()) {
+			resultList.add(depthFirstIterator.next().toString());
+		}
+		return resultList;
+	}
+	
 	public String[] parseLine(String line) {
 		String[] tokens = line.split("=>");
 		if (tokens.length == 2) {
@@ -34,6 +72,7 @@ public class JobDependencyResolver {
 		InputStreamReader r=new InputStreamReader(System.in);    
 		BufferedReader br=new BufferedReader(r); 
 		String line = null;
+		List<String> resultList;
 		try {
 			while((line = br.readLine()) != null) {
 				if (line.equalsIgnoreCase("stop")) {
@@ -41,9 +80,15 @@ public class JobDependencyResolver {
 				} else {
 					String[] tokens = parseLine(line);
 					if (tokens != null) {
-						//addEdge();
+						inputList.add(tokens);
 					}
 				}
+			}
+			if (inputList.isEmpty()) {
+				System.out.println();
+			} else {
+				resultList = scheduleJobs(inputList);
+				printResult(resultList);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -51,7 +96,12 @@ public class JobDependencyResolver {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public void stop() {
+		if (inputList != null)
+			inputList.clear();
+	}
+	
 	public static void main(String[] args) {
 		JobDependencyResolver jobResolver = new JobDependencyResolver();
 		jobResolver.processInput();
--- src/utils/GraphUtils.java
+++ src/utils/GraphUtils.java
@@ -1,11 +1,15 @@
 package utils;
 
+import java.util.Set;
+
+import org.jgrapht.alg.cycle.CycleDetector;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
 public class GraphUtils {
 	
-	public void createGraph() {
-
-	}
-
+	
+	
 	public void isGraphEmpty() {
 
 	}
