875d7adae8d06314a8a5dbe8c1c87bf299e31d79
ankitmarothi1610 <ankitmarothi1610@gmail.com>
2019-06-16 19:47:41 +0530

    Initial code structure
    Adding initial code structure, seperating utility methods and actual
    code

---
+++ src/Resolver/JobDependencyResolver.java
@@ -0,0 +1,27 @@
+package Resolver;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobDependencyResolver {
+	List<String> inputList;
+	public JobDependencyResolver() {
+		inputList = new ArrayList<String>(); 
+	}
+	
+	public void parseInput() {
+		
+	}
+	
+	public void processInput() {
+		
+	}
+	
+	public void printResult() {
+		
+	}
+	
+	public static void main(String[] args) {
+		JobDependencyResolver jobResolver = new JobDependencyResolver();
+		jobResolver.parseInput();
+	}
+} 
---
+++ src/utils/GraphUtils.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class GraphUtils {
+	public void createGraph() {
+
+	}
+
+	public void isGraphEmpty() {
+
+	}
+}
